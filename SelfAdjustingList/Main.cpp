51565dca3e051c7447128069f7cdbd46f2bf6be4
Stephen L Belden <belden.stephen@gmail.com>
2015-09-16 21:52:01 -0600

    Adjustments to Intro Text
    Changed Cluster Radius to Cluster Size.
    Explained what the cluster search was doing.
    Made the whole thing a bit shorter.
    Tried to simplify the wording of a bunch of things.

--- SelfAdjustingList/Main.cpp
+++ SelfAdjustingList/Main.cpp
@@ -13,36 +13,32 @@ int main() {
 	while (1) {
 		int inputSize = 0;
 		int upperBound = 0;
-		int clusterRadius = 0;
+		int clusterSize = 0;
 
 		// Only display this intro text on the first run through the program
 		if (cycles <= 0) {
-			std::cout << "This program demonstrates the efficiency of self-adjusting lists in certain" << std::endl;
-			std::cout << "situations by generating a random list of unique ints, then searching through" << std::endl;
-			std::cout << "that list both with and without the self-adjusting function of the list." << std::endl;
-			std::cout << "The same initial random order of elements is used for both tests." << std::endl;
-			std::cout <<  "The total and average number of steps taken to search will be returned." << std::endl ;
-			std::cout <<  "A step is counted each time a new element is checked at while searching.";
+			std::cout << "This program demonstrates the efficiency of self-adjusting lists in clustered" << std::endl;
+			std::cout << "searches by generating a random list of unique ints, then repeatedly searching" << std::endl;
+			std::cout << "through that list for groups of sequential numbers. For example, a Cluster Size" << std::endl;
+			std::cout << "of 4 would search for the following numbers in this order:" << std::endl;
+			std::cout << "1,2,3,4, 2,3,4,5, 3,4,5,6, ... until the maximum value is reached." << std::endl;
+			std::cout << "The total and average number of steps taken to search are displayed." << std::endl;
+			std::cout << "A step is counted each time a new element is visited in the list." << std::endl;
 			std::cout << std::endl;
 			std::cout << "Explination of input variables:" << std::endl;
-			std::cout << "* List Length -    The length of the list. Must be non-negative integer." << std::endl;
-			std::cout << "                   (Recommended value 0-1000)" << std::endl;
-			std::cout << "* Max Value -      The list will be filled with unique, non-repeating, random" << std::endl;
-			std::cout << "                   numbers from 1 to this value." << std::endl;
-			std::cout << "                   Must be greater than or equal to List Length." << std::endl;
-			std::cout << "* Cluster Radius - While searching, the number being searched for may increace" << std::endl;
-			std::cout << "                   or decreace by a maximum of this ammount. This value also" << std::endl;
-			std::cout << "                   controls how long the cluster is centered around one value" << std::endl;
-			std::cout << "                   before going on to the next, so making this value larger will" << std::endl;
-			std::cout << "                   result in more find calls." << std::endl;
-			std::cout << "                   (Recommended values 1-10, unless the list is small)" << std::endl;
+			std::cout << "* List Length:  Number of elements in the list. Must be non-negative integer." << std::endl;
+			std::cout << "                (Recommended value 1-1000; inputing 0 currently doesn't work.)" << std::endl;
+			std::cout << "* Max Value:    The list will be filled with non-repeating, random numbers from" << std::endl;
+			std::cout << "                1 to this value. MUST be greater than or equal to List Length." << std::endl;
+			std::cout << "* Cluster Size: The number of sequential elements searched for before moving on" << std::endl;
+			std::cout << "                to the next series. (Recommended values 1-30)" << std::endl;
 			std::cout << std::endl;
 			std::cout << "Some interesting values to try:" << std::endl;
-			std::cout << "* Max Value 1 less than List Length - ideal situation with no misses" << std::endl;
-			std::cout << "* Max value much greater than list length - more misses than hits" << std::endl;
-			std::cout << "* Cluster size of 1 - plain linear search, no searches repeated" << std::endl;
-			std::cout << "* Large cluster size - more opportunities for the self-adjusting function to" << std::endl;
-			std::cout << "                     work effectively, which should widen the gap between tests." << std::endl;
+			std::cout << "* Max Value equal to List Length - ideal situation with no misses" << std::endl;
+			std::cout << "* Max Value much greater than List Length - more misses than hits" << std::endl;
+			std::cout << "* Cluster Size of 1 - plain linear search, no searches repeated" << std::endl;
+			std::cout << "* Large Cluster Size - more opportunities for the self-adjusting function to" << std::endl;
+			std::cout << "                     work effectively, which should widen the gap between tests" << std::endl;
 			std::cout << std::endl;
 		}
 
@@ -51,8 +47,8 @@ int main() {
 		std::cin >> inputSize;
 		std::cout << "Max Value: ";
 		std::cin >> upperBound;
-		std::cout << "Cluster Radius: ";
-		std::cin >> clusterRadius;
+		std::cout << "Cluster Size: ";
+		std::cin >> clusterSize;
 		std::cout << std::endl;
 
 		// initialization
@@ -60,7 +56,7 @@ int main() {
 		A.uniqueRandomFill(inputSize, 1, upperBound);
 
 		// Regular linear search
-		if (clusterRadius <= 1) {
+		if (clusterSize <= 1) {
 			{ // new block to handle creating and deleting temp variables
 				std::cout << "Searching for numbers 1 to Max Value once each, WITHOUT self-adjusting..." << std::endl;
 				int findCalls = 0;
@@ -111,7 +107,7 @@ int main() {
 				int found = 0;
 				long steps = 0;
 				while (current <= upperBound) {
-					for (int i = 0; i < clusterRadius; i++) {
+					for (int i = 0; i < clusterSize; i++) {
 						int temp = A.check(current + i);
 						findCalls++;
 						if (temp >= 1) {
@@ -135,7 +131,7 @@ int main() {
 				int found = 0;
 				long steps = 0;
 				while (current <= upperBound) {
-					for (int i = 0; i < clusterRadius; i++) {
+					for (int i = 0; i < clusterSize; i++) {
 						int temp = A.find(current + i);
 						findCalls++;
 						if (temp >= 1) {
